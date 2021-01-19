bbb43a70e72144ffc4f4bd223d415bccd5b0be2d
mega98 <megakrismiyati@gmail.com>
2021-01-19 19:46:50 +0700

    Initial commit

---
+++ system/app/Http/Controllers/HomeController.php
@@ -0,0 +1,50 @@
+<?php 
+
+namespace App\Http\Controllers;
+
+use App\Models\Produk;
+
+class HomeController extends Controller{
+
+
+	function showdashboard(){
+		return view('dashboard');
+	}
+
+	function showproduk(){
+		return view('produk');
+	}
+
+	function showkategori(){
+		return view('kategori');
+	}
+
+	function showhome(){
+		return view('home');
+	}
+
+	function showproduk1(){
+		return view('produk1');
+	}
+
+	function showcheckout(){
+		return view('checkout');
+	}
+
+	function showabout(){
+		return view('about');
+	}
+
+	function showblog(){
+		return view('blog');
+	}
+
+	function showcontact(){
+		return view('contact');
+	}
+
+	function showcart(){
+		return view('cart');
+	}
+
+}
